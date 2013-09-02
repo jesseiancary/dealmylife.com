daaa56e3857a74764a573e8853f09ab8b0017334
Jesse Ian Cary <jesseiancary@gmail.com>
2013-09-02 14:54:47 -0700

    Updated some navigations.

--- www/app/controllers/users_controller.rb
+++ www/app/controllers/users_controller.rb
@@ -103,7 +103,7 @@ class UsersController < ApplicationController
 		respond_to do |format|
 			if @user.update_attributes(params[:user])
 				#format.html { post_to :controller => "account", action: "logout", notice: "User Account has been updated." }
-        format.html { redirect_to users_url, notice: "Password has been successfully updated." }
+        format.html { redirect_to profile_url, notice: "Password has been successfully updated." }
 				format.json	{ head :ok }
 			else
 				format.html { render action: "edit" }
