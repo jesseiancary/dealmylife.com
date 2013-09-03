bebb8522ce4ee7c6f950f7d0cd0554636dd9faac
Jesse Ian Cary <jesseiancary@gmail.com>
2013-09-02 17:38:42 -0700

    Updated Living Social interface.

--- www/lib/assets/partners/living-social.rb
+++ www/lib/assets/partners/living-social.rb
@@ -1,12 +1,10 @@
 require "open-uri"
 require "json"
-#require "date"
 
 module Api
   
   class Deals
     attr_accessor :deals
-    #protected
     
     def initialize(partner, address)
       @partner = partner
@@ -19,7 +17,7 @@ module Api
       response = @uri.open
       raise "web service error" if response.status.first != "200"
       
-      return JSON.parse response.read
+      @deals = JSON.parse response.read
       
     end
     
@@ -38,32 +36,30 @@ module Api
           
           response["deals"].each do | deal |
             
-            begin
+            #begin
               
               option_index = 0
               option_price = 0
               deal["options"].each_with_index do | option, i |
-                if option["price"]["amount"] < option_price || i == 0
+                if option["price"] < option_price || i == 0
                   option_index = i
                 end
               end
-              
+                
               locations = []
-              deal["options"][option_index]["redemptionLocations"].each do | location |
-                l = {
-                  :neighborhood => location["neighborhood"],
-                  :address1 => location["streetAddress1"],
-                  :address2 => location["streetAddress2"],
-                  :city => location["city"],
-                  :state => location["state"],
-                  :zip => location["postalCode"],
-                  :country => location["country"],
-                  :phone => location["phoneNumber"],
-                  :longitude => location["lng"],
-                  :latitude => location["lat"]
-                }
-                locations.push l
-              end
+              l = {
+                :neighborhood => "",
+                :address1 => "",
+                :address2 => "",
+                :city => "",
+                :state => "",
+                :zip => "",
+                :country => "",
+                :phone => "",
+                :longitude => "",
+                :latitude => ""
+              }
+              locations.push l
               
               category = PartnerCategory.get_category( if deal["tags"].is_a? Array and deal["tags"].length > 0 then deal["tags"][0]["name"] else "" end )
                 
@@ -71,24 +67,24 @@ module Api
                 
                 :partner => @partner.name,
                 :partner_id => @partner.id,
-                :id => "groupon-" + deal["id"],
-                :title => deal["announcementTitle"],
+                :id => "livingsocial-" + deal["id"].to_s,
+                :title => deal["short_title"],
                 
-                :imageUrl => deal["sidebarImageUrl"],
-                :dealUrl => deal["dealUrl"],
-                :pitchHtml => deal["pitchHtml"],
-                :highlightsHtml => deal["highlightsHtml"],
-                :description => if deal["options"][option_index]["details"].length > 0 then deal["options"][option_index]["details"][0]["description"] else "" end,
+                :imageUrl => deal["images"][0]["size220"],
+                :dealUrl => deal["url"],
+                :pitchHtml => deal["long_title"],
+                :highlightsHtml => "",
+                :description => deal["options"][option_index]["description"],
                 
-                :sold => deal["options"][option_index]["soldQuantity"].to_i || 0,
-                :begin => Time.parse(deal["tippedAt"] || Time.now.to_s).to_s,
-                :end => Time.parse(deal["endAt"] || Time.now.to_s).to_s,
-                :remaining => ((Time.parse(deal["endAt"] || Time.now.to_s) - Time.now)/86400).to_i,
+                :sold => deal["orders_count"].to_i || 0,
+                :begin => Time.parse(deal["offer_starts_at"] || Time.now.to_s).to_s,
+                :end => Time.parse(deal["offer_ends_at"] || Time.now.to_s).to_s,
+                :remaining => ((Time.parse(deal["offer_ends_at"] || Time.now.to_s) - Time.now)/86400).to_i,
                 
-                :price => deal["options"][option_index]["price"]["amount"],
-                :value => deal["options"][option_index]["value"]["amount"],
-                :discountAmount => deal["options"][option_index]["discount"]["amount"].to_i || 0,
-                :discountPercent => deal["options"][option_index]["discountPercent"].to_i || 0,
+                :price => deal["options"][option_index]["price"]*100,
+                :value => deal["options"][option_index]["original_price"]*100,
+                :discountAmount => deal["options"][option_index]["savings"]*100 || 0,
+                :discountPercent => deal["options"][option_index]["discount"] || 0,
                 
                 :categoryId => category.id,
                 :category => category.name,
@@ -98,9 +94,9 @@ module Api
               }
               @deals.push d
             
-            rescue
-              puts "Deal Parse Error - " + @partner.name
-            end
+            #rescue
+            #  puts "Deal Parse Error - " + @partner.name
+            #end
             
           end
           
